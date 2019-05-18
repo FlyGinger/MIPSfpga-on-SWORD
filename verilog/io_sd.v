c837700b105588338e6e0877b4a6acb17a2c09dc
FlyGinger <38899898+FlyGinger@users.noreply.github.com>
2019-05-18 16:18:19 +0800

    create sd

---
+++ verilog/io_sd.v
@@ -0,0 +1,21 @@
+// Author: Jiang Zengkai
+// Date: 2019.5.18
+
+module io_sd(
+    input wire clk, // 100 MHz
+    input wire rst, // reset, active high
+    output wire sd_clk,
+    input wire sd_cd, // card detect, not used
+    inout wire sd_cmd, // command
+    inout wire [3:0] sd_dat // data
+);
+
+    // SD card clock generation
+    
+
+
+
+
+
+
+endmodule
