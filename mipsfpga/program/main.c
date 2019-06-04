287e9e5632b03d04b8367e03412a0e74df46d59f
zengkai <zengkai97@outlook.com>
2019-06-04 14:10:21 +0800

    v5.3 最终版本

--- mipsfpga/program/main.c
+++ mipsfpga/program/main.c
@@ -44,54 +44,38 @@ int main() {
     }
     set_arduino_leds(0x1);
 
-    // int sector = 0;
-    // while (1) {
-    //     i = 100000;
-    //     while (i-- > 0)
-    //         set_seg7led(i);
-    //     vga_clear();
-    //     sd_read_block(buf, sector++, 1);
-    //     int i;
-    //     for (i = 0; i < 512; i++) {
-    //         put_char(hex2char(buf[i] & 0xf));
-    //         put_char(hex2char(buf[i] >> 4));
-    //     }
-    //     for (i = 0; i < 16; i++) {
-    //         put_char(0);
-    //     }
-    // }
+    while (1) {
+        unsigned int sw = get_switches();
+        set_leds(sw);
 
-    // while (1) {
-    //     unsigned int sw = get_switches();
-    //     set_leds(sw);
+        if (sw & 0x1) {
+            set_arduino_buz(95420); // C
+        } else if (sw & 0x2) {
+            set_arduino_buz(89928); // C#
+        } else if (sw & 0x4) {
+            set_arduino_buz(85034); // D
+        } else if (sw & 0x8) {
+            set_arduino_buz(80386); // D#
+        } else if (sw & 0x10) {
+            set_arduino_buz(75758); // E
+        } else if (sw & 0x20) {
+            set_arduino_buz(71633); // F
+        } else if (sw & 0x40) {
+            set_arduino_buz(67568); // F#
+        } else if (sw & 0x80) {
+            set_arduino_buz(63776); // G
+        } else if (sw & 0x100) {
+            set_arduino_buz(60241); // G#
+        } else if (sw & 0x200) {
+            set_arduino_buz(56818); // A
+        } else if (sw & 0x400) {
+            set_arduino_buz(53648); // A#
+        } else if (sw & 0x800) {
+            set_arduino_buz(50607); // B
+        } else {
+            set_arduino_buz(0); // silent
+        }
+    }
 
-    //     if (sw & 0x1) {
-    //         set_arduino_buz(95420); // C
-    //     } else if (sw & 0x2) {
-    //         set_arduino_buz(89928); // C#
-    //     } else if (sw & 0x4) {
-    //         set_arduino_buz(85034); // D
-    //     } else if (sw & 0x8) {
-    //         set_arduino_buz(80386); // D#
-    //     } else if (sw & 0x10) {
-    //         set_arduino_buz(75758); // E
-    //     } else if (sw & 0x20) {
-    //         set_arduino_buz(71633); // F
-    //     } else if (sw & 0x40) {
-    //         set_arduino_buz(67568); // F#
-    //     } else if (sw & 0x80) {
-    //         set_arduino_buz(63776); // G
-    //     } else if (sw & 0x100) {
-    //         set_arduino_buz(60241); // G#
-    //     } else if (sw & 0x200) {
-    //         set_arduino_buz(56818); // A
-    //     } else if (sw & 0x400) {
-    //         set_arduino_buz(53648); // A#
-    //     } else if (sw & 0x800) {
-    //         set_arduino_buz(50607); // B
-    //     } else {
-    //         set_arduino_buz(0); // silent
-    //     }
-    // }
     return 0;
 }
